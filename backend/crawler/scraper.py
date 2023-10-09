9a0f13a93c1d4aacb6ef9efebc3d0a4d80612dce
Giovanni Ciccia <giovanniciccia1@gmail.com>
2023-10-09 18:18:52 -0500

    se puso scraper en su propio file

---
+++ backend/crawler/scraper.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+from utils import pre_process_text
+
+class Scraper:
+    def __init__(self, response):
+        self.url = response.url if not response.history else response.history[0].url
+        self.soup  = BeautifulSoup(response.content, 'html.parser')
+        self.lang_code = self.soup.html.get("lang")
+        self.title = "" if not self.soup.title else self.soup.title.string
+        self.description_meta = self.soup.find("meta", property="og:description")
+        self.text = self.soup.get_text()
+        self.first_p = self.soup.find('p')
+        self.first_p_text = None if not self.first_p else self.first_p.text
+        self.description_text = self.first_p_text if not self.description_meta else self.description_meta.content
+
+    def get_urls(self):
+        urls = []
+        for anchor in self.soup.find_all('a'):
+            urls.append(anchor.get('href'))
+        return urls
+
+    def to_dict(self):
+        return {
+            "url": self.url,
+            "title": None if not self.title else pre_process_text(self.title, max_len=280),
+            "description": pre_process_text(self.description_text, max_len=280)
+        }
+
+    
--- backend/crawler/web_crawler.py
+++ backend/crawler/web_crawler.py
@@ -5,6 +5,7 @@ from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import Session
 from utils import pre_process_text
 import os
+from scraper import Scraper
 
 INDEXER_URL = os.environ.get("INDEXER_URL")
 
@@ -20,33 +21,20 @@ while count < 1000 and len(queue) > 0:
     except:
         continue
     if result.status_code // 100 != 2: continue
-    content = result.content
-    soup = BeautifulSoup(content, 'html.parser')
-    lang_code = soup.html.get("lang")
-    if lang_code is None or not (lang_code.startswith("en") or lang_code.startswith("es")): continue
-    title = soup.title
-    description = soup.find("meta", property="og:description")
-    text = soup.get_text()
-    first_p = soup.find('p')
-    first_p = None if not first_p else first_p.text
-    description = first_p if not description else description.content
-    parsed_site = {
-        "url": site_url, 
-        "title": None if not title else pre_process_text(title.string, max_len=280),
-        "description": pre_process_text(description, max_len=280)
-    }
+    scraper = Scraper(result)
+    if scraper.lang_code is None or not (scraper.lang_code.startswith("en") or scraper.lang_code.startswith("es")): continue
+    parsed_site = scraper.to_dict()
     stmt = insert(Page).values(**parsed_site).on_conflict_do_update(index_elements=["url"], set_=parsed_site)
     with Session(engine) as session:
         result = session.execute(stmt)
         session.commit()
         inserted_id = result.inserted_primary_key[0]
-    for anchor in soup.find_all('a'):
-        link = anchor.get('href')
+    for link in scraper.get_urls():
         if link and link not in visited and link.startswith("http") and len(link) <= 100:
             queue.append(link)
             visited.add(link)
     try:
-        response = requests.post(INDEXER_URL, json={"id": inserted_id, "title": parsed_site["title"], "text": text})
+        response = requests.post(INDEXER_URL, json={"id": inserted_id, "title": scraper.title, "text": scraper.text})
     except Exception as e:
         print(e)
     count += 1
