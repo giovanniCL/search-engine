caacdc84a44b4c91cede2c222b1169650481f08c
Giovanni Ciccia <giovanniciccia1@gmail.com>
2023-10-09 18:31:00 -0500

    se abstrajo cliente de db

--- backend/crawler/web_crawler.py
+++ backend/crawler/web_crawler.py
@@ -1,17 +1,17 @@
-from bs4 import BeautifulSoup
 import requests
 from db import Page, engine
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import Session
-from utils import pre_process_text
 import os
 from scraper import Scraper
+from sqlalchemy_client import SqlAlchemyClient
 
 INDEXER_URL = os.environ.get("INDEXER_URL")
 
 queue = ["https://www.reddit.com/"]
 visited = set("https://www.reddit.com/")
 count = 0
+db_client = SqlAlchemyClient(engine, Page)
 while count < 1000 and len(queue) > 0:
     site_url = queue.pop()
     try:
@@ -24,11 +24,7 @@ while count < 1000 and len(queue) > 0:
     scraper = Scraper(result)
     if scraper.lang_code is None or not (scraper.lang_code.startswith("en") or scraper.lang_code.startswith("es")): continue
     parsed_site = scraper.to_dict()
-    stmt = insert(Page).values(**parsed_site).on_conflict_do_update(index_elements=["url"], set_=parsed_site)
-    with Session(engine) as session:
-        result = session.execute(stmt)
-        session.commit()
-        inserted_id = result.inserted_primary_key[0]
+    inserted_id = db_client.create_or_update_page(parsed_site)
     for link in scraper.get_urls():
         if link and link not in visited and link.startswith("http") and len(link) <= 100:
             queue.append(link)
